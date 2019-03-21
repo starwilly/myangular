b987fe5ebd986ef89d7b1a442ed410309492e17f
starwilly <willystar@gmail.com>
2019-03-22 00:59:52 +0800

    compare watch value and trigger listener function

--- src/scope.js
+++ src/scope.js
@@ -2,16 +2,25 @@ export function Scope() {
     this.$$watchers = [];
 }
 
+function initWatchVal() {}
+
 Scope.prototype.$watch = function (watchFn, listenerFn) {
     const watcher = {
         watchFn,
-        listenerFn
+        listenerFn,
+        last: initWatchVal
     };
     this.$$watchers.push(watcher);
 }
 
 Scope.prototype.$digest = function () {
+    const self = this;
     this.$$watchers.forEach(watcher => {
-        watcher.listenerFn();
+        let newValue = watcher.watchFn(self);
+        let oldValue = watcher.last;
+        if (newValue !== oldValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue, oldValue, self);
+        }
     })
 }
--- test/scope.spec.js
+++ test/scope.spec.js
@@ -5,11 +5,6 @@ import {
 } from '../src/scope';
 
 describe('Scope', () => {
-    var scope;
-
-    beforeEach(() => {
-        scope = new Scope();
-    });
 
     it('can be construct and used as an object', () => {
         const scope = new Scope();
@@ -18,12 +13,76 @@ describe('Scope', () => {
         expect(scope.aProperty).toBe(1);
     });
 
-    it('calls the listener function of a watch on first $digest', () => {
-        const watchFn = () => 'wat';
-        const listenerFn = jest.fn()
-        scope.$watch(watchFn, listenerFn);
-        scope.$digest();
+    describe('digest', () => {
+        let scope;
 
-        expect(listenerFn).toBeCalled();
+        beforeEach(() => {
+            scope = new Scope();
+        });
+
+        it('calls the listener function of a watch on first $digest', () => {
+            const watchFn = () => 'wat';
+            const listenerFn = jest.fn()
+            scope.$watch(watchFn, listenerFn);
+            scope.$digest();
+
+            expect(listenerFn).toBeCalled();
+        });
+
+        it('calls the watch function with the scope as an argument ', () => {
+            const watchFn = jest.fn();
+            const listenerFn = jest.fn();
+            scope.$watch(watchFn, listenerFn);
+
+            scope.$digest();
+
+            expect(watchFn).toBeCalledWith(scope);
+        });
+
+        it('call the listener function when the watched value changes', () => {
+            scope.someValue = 'a';
+            scope.counter = 0;
+
+            scope.$watch(
+                function (scope) {
+                    return scope.someValue
+                },
+                function (newValue, oldValue, scope) {
+                    scope.counter++
+                }
+            );
+
+            expect(scope.counter).toBe(0);
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+
+            scope.someValue = 'b'
+            expect(scope.counter).toBe(1);
+
+            scope.$digest();
+            expect(scope.counter).toBe(2);
+        });
+
+        it('calls listener when watch value is first undefined', () => {
+            scope.counter = 0;
+
+            scope.$watch(
+                function (scope) {
+                    return scope.someValue
+                },
+                function (newValue, oldValue, scope) {
+                    scope.counter++
+                }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
+
     });
+
 });
